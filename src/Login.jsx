e57ad0892a2a0336b3c45215478a13b694510295
Fathimathul Faiza <fathimthulfaiza@gmail.com>
2025-12-10 14:57:06 +0530

    connecting login Api from backend to frontend axios, CORS

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios"
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
 
-export default Login
+  // These will store what the user types
+  const [emailId, setEmailId] = useState("fathimthulfaiza@gmail.com");
+  const [password, setPassword] = useState("Faiza@123");
+
+// handle the API call - Axios used (network call)
+
+   const handleLogin = async ()=>{
+
+    try{
+      const res = await axios.post("http://localhost:7777/login", {
+        emailId,
+        password,
+       
+      }, { withCredentials: true }  )
+
+    }
+    catch(err){
+      console.error(err)
+    }
+   }
+
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+
+          <div>
+            {/* Email */}
+            <fieldset className="fieldset w-full">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                className="input"
+                placeholder="Type your Email ID here"
+                value={emailId}   // show the value
+                onChange={(e) => setEmailId(e.target.value)} // update the value
+              />
+            </fieldset>
+
+            {/* Password */}
+            <fieldset className="fieldset w-full">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="password"
+                className="input"
+                placeholder="Type your Password here"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
