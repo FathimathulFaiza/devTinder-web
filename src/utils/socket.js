3babaa71e9956408920cf32fd070a7fd1ca8cca7
Fathimathul Faiza <fathimthulfaiza@gmail.com>
2026-01-02 15:36:11 +0530

    fix: removed port 7777 to allow nginx proxying

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,11 +2,12 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL =
   import.meta.env.MODE === "production"
-    ? "https://devpartner.work:7777" // Change httpss and add :7777
+    ? "https://devpartner.work" // Change httpss and add :7777
     : "http://localhost:7777";
 
 export const createSocketconnection = () => {
   return io(SOCKET_URL, {
+    path: "/socket.io",
     withCredentials: true,
   });
 };
